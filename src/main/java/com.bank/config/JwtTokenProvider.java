1a8f6c42f330512ea5db9d8d7c6a88152e6eb91e
kanupriya0077 <74521446+kanupriya0077@users.noreply.github.com>
2025-05-20 03:25:12 +0530

    Create JwtTokenProvider.java

---
+++ src/main/java/com.bank/config/JwtTokenProvider.java
@@ -0,0 +1,62 @@
+package com.bank.Bank.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value(("${app.jwt-expiration}"))
+    private long jwtExpirationDate;
+
+    public String generateToken(Authentication authentication){
+        String userName=authentication.getName();
+        Date currentdate=new Date();
+        Date expiryDate=new Date(currentdate.getTime()+jwtExpirationDate);
+
+        return Jwts.builder()
+                .setSubject(userName)
+                .setIssuedAt(currentdate)
+                .setExpiration(expiryDate)
+                .signWith(key())
+                .compact();
+    }
+    private Key key(){
+        byte[] bytes= Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+    public String getUserName(String token){
+        Claims claims=Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parse(token);
+            return true;
+        } catch (ExpiredJwtException | IllegalArgumentException | SignatureException | MalformedJwtException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
